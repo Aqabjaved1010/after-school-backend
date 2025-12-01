0fbbb98bb1e180b6dadff53f4894921efb614a0e
Aqab Javed <aqabj1010@gmail.com>
2025-12-01 22:32:43 +0000

    Added request logger middleware

--- server.js
+++ server.js
@@ -11,6 +11,12 @@ const dbName = process.env.DB_NAME || 'after_school'
 
 let db
 
+app.use((req, res, next) => {
+  console.log(`${new Date().toISOString()} ${req.method} ${req.url}`)
+  next()
+})
+
+
 app.use(cors())
 app.use(express.json())
 
